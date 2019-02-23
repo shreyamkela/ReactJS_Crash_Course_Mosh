0268f0e5d1e6f3ba047819679a86f84fe7ffbf6a
shrke <shreyamkela@gmail.com>
2019-02-22 20:36:32 -0800

    bootstrap button

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,14 @@ import React, { Component } from "react"; // using { Component } with object des
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    imageURL: "https://picsum.photos/200" // generate random image and pass into jsx
+  };
+
+  styles = {
+    // CSS styles object to be passed into jsx. Properties are written in camel case.
+    fontSize: 10,
+    fontWeight: "bold"
   };
 
   render() {
@@ -13,16 +20,29 @@ class Counter extends Component {
     //     <h1> Hello World </h1>
     //     <button> Increment </button>
     //   </React.Fragment>
-    return (
-      // using <div>
-      <div>
-        <span>{this.formatCount()}</span>
-        <button> Increment </button>
-      </div>
-    );
-  } // If we use <span>{this.formatCount()}</span> without the {} then it will print on the browser this.formatCount() as string. Therefore we need to destructure the object as we are using jsx/html and not plain js here.
+    // );
+    // return ( // using <div> and not React.Fragment
+    // The below passes an image into jsx
+    // <div>
+    //   <img src={this.state.imageURL} alt="" />
+    //   <span>{this.formatCount()}</span>
+    //   <button> Increment </button>
+    // </div>
+    // The below passes a class into jsx
+    // The below passes bootstrap class
+    // <div>
+    //   <span style={this.styles} className="badge badge-primary m-2">
+    //     {this.formatCount()}
+    //   </span>
+    //   <button className="btn btn-secondary btn-sm"> Increment </button>
+    // </div>
+    // ); // className is keyword for passing class into jsx. badge is bootstrap class and m-2 provides 2 margin to badge. btn is button class and sm means small. the style attribute inside span is used to pass style sheet object into jsx. If we only need to pass say just 1 style then we can also use this syntax - style={ {fontSize: 30} } i.e directly entering the style object into jsx
+    // If we use <span>{this.formatCount()}</span> without the {} then it will print on the browser this.formatCount() as string. Therefore we need to destructure the object as we are using jsx/html and not plain js here.
+    // The HTML <span> tag is used for grouping and applying styles to inline elements. There is a difference between the span tag and the div tag. The span tag is used with inline elements whilst the div tag is used with block-level content.
+  }
 
   formatCount() {
+    // Can also pass functions into jsx
     const { count } = this.state; // Save value of the count key of this.state into a count variable - object destructuring
     return count === 0 ? "Zero" : count;
   }
