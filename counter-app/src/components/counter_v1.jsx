7f33146c9e6edf08365c404046922b2628f1ce42
shrke <shreyamkela@gmail.com>
2019-02-23 17:19:29 -0800

    deleting a counter

--- counter-app/src/components/counter_v1.jsx
+++ counter-app/src/components/counter_v1.jsx
@@ -1,12 +1,12 @@
 // There are 4 counter components inside 1 counters component
 
-import React, { Component } from "react"; // using { Component } with object destructuring - https://stackoverflow.com/questions/41768205/difference-between-import-react-and-import-component-syntax , https://medium.com/@AnnaJS15/a-simple-guide-to-destructuring-props-in-react-f02e6e51143a
+import React, { Component } from "react"; // using { Component } with object destructuring - https://stackoverflow.com/questions/41768205/difference-between-import-react-and-import-component-syntax , https://medium.com/@AnnaJS15/a-simple-guide-to-destructuring-props-in-react-f02e6e51143a. For eg, in the object { key: value } if key and value variables have the same name then it can be just written as { key }
 
 class Counter extends Component {
   state = {
-    // count: 0, // NOTE count is renamed to value when adding this.props.value
-    value: this.props.value, // value has been set for each counter in the Counters component and can be used here
-    id: this.props.id // Using the id set by props
+    // count: 0, // NOTE count is renamed to value when adding this.props.counter.value. Counters class is sending into counter class instance a prop counter which has the id, value, etc of the current counter
+    value: this.props.counter.value, // value has been set for each counter in the Counters component and can be used here
+    id: this.props.counter.id // Using the id set by props
     // tags: []
   }; // Difference between props and state - Props are read only objects that are associated with objects, and props of one components can be read by other components. Props can be sent as inputs to a component. Props are defined when we use an object in JSX. For eg, <Counter prop1="..." prop2="..."> ... </Counter>. Input porps can set the state. State is readable and writable (setState()) but is local to a component. State of one component cannot be accessed by other components
 
@@ -90,7 +90,10 @@ class Counter extends Component {
         {/* {this.state.tags.length === 0 && "Please create a new tag!"} */}
         {/* This is using if else with &&, directly inside JSX. In Javascript, we can apply logical operators even on non-boolean objects. The && and || operators actually return the value of one of the specified operands, so if these operators are used with non-Boolean values, they will return a non-Boolean value - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators . This uses the Truthy and falsy concept of javascript - https://stackoverflow.com/questions/35642809/understanding-javascript-truthy-and-falsy . In truthy, if all the operands are true, then the final operand is the result*/}
         <button
-          onClick={this.props.onDelete}
+          // onClick={this.props.onDelete}
+          onClick={() => {
+            this.props.onDelete(this.props.counter.id);
+          }}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
@@ -98,6 +101,7 @@ class Counter extends Component {
         {/* NOTE In React -  The component that owns a piece of the state, should be the one modifying it. Therefore we cannot have a counter_v1 component deleting itself from the state of counters (when we want to delete a counter component from counters). Thus we have to include the delete logic into counters itself. Different counters are listed in the state of counters class and if we want to remove any then delete logic should also be in the counters class only  */}
         {/*We have used onDelete={this.handleDelete} in Counters class, to register an event handler prop that passes a reference to this.handleDelete so that counter object can refer to this method of the counters class. We could not have added handleDelete method in counter class itself as handleDelete manipulates the state of counters class and the component that owns a piece of the state, should be the one modifying it. Now in counter class, we can reference this method for the current object and use this event handler for the deletion of that particular counter object. This is raising and handling of events. Counter class will raise an event and Counters class will handle that event. Counters class provides the handleDelete method which can be instantiated by counter object for the current object and can be used to delete the object.*/}
         {/* Thus onClick will make a reference or instantiate the handleDelete method of Counters class for this object as onDelete has been set as a prop in Counters class */}
+        {/* We us the arrow function this.props.onDelete(this.props.counter.id) so as to pass the id of the counter object that we want to delete, to the Counters object */}
       </div>
     );
   }
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -11,8 +11,16 @@ class Counters extends Component {
     ]
   };
 
-  handleDelete = () => {
-    console.log("Event Handler Called");
+  handleDelete = counterID => {
+    // console.log("Event Handler Called", counterID);
+    const counters = this.state.counters.filter(c => c.id !== counterID);
+    /* 
+    Here weare filtering out the key counterID from the counters list in the state of counters class. We have to remove counterID key-value pair from the counters list in the state, but we cannot do this by directly manipulating the state and set the state ourselves. In react, we let react change the state using setState, otherwise react wont be able to register the change. 
+    To filter out the counterID key value pair from the counters list, we make a new const counters and save into it all the key-value pairs (of counters list) except the counterID key value pair. c => c.id !==counterID is defining c as an iterator variable to iterate over the list and filter out the counterID form the list - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter.
+    Thus we do not change the state directly, when a counter is deleted. Rather we make a new counters list and override the old counters list using this new counters list, with the setState method. With setState, react knows that there is a change in state and therefore will register it render again.
+    */
+    // this.setState({ counters: counters }); // Can also be written as below
+    this.setState({ counters }); // Destructuring
   };
 
   render() {
@@ -23,17 +31,23 @@ class Counters extends Component {
         <Counter />
         <Counter /> Instead of hardcoding 4 Counter components to pass them here, we can set 4 counters in the state and then pass that state object here. THis is done below*/}
         {this.state.counters.map(counter => (
-          <Counter
+          /* <Counter
             key={counter.id}
             value={counter.value}
             id={counter.id}
             onDelete={this.handleDelete}
-          /> // We have used onDelete={this.handleDelete} to register an event handler prop that passes a reference to this.handleDelete so that counter object can refer to this method of the counters class. We could not have added handleDelete method in counter class itself as handleDelete manipulates the state of counters class and the component that owns a piece of the state, should be the one modifying it. Now in counter class, we can reference this method for the current object and use this event handler for the deletion of that particular counter object. This is raising and handling of events. Counter class will raise an event and Counters class will handle that event. Counters class provides the handleDelete method which can be instantiated by counter object for the current object and can be used to delete the object.
+          /> We have used onDelete={this.handleDelete} to register an event handler prop that passes a reference to this.handleDelete so that counter object can refer to this method of the counters class. We could not have added handleDelete method in counter class itself as handleDelete manipulates the state of counters class and the component that owns a piece of the state, should be the one modifying it. Now in counter class, we can reference this method for the current object and use this event handler for the deletion of that particular counter object. This is raising and handling of events. Counter class will raise an event and Counters class will handle that event. Counters class provides the handleDelete method which can be instantiated by counter object for the current object and can be used to delete the object. */
           // Can also add selected = true tag here - https://stackoverflow.com/questions/21733847/react-jsx-selecting-selected-on-selected-select-option
-          // This is to send children props to the counter object. This can be used for example when we distribute components of dialog boxes to consumers and they can specify their own warning messages in the dialog box components through this children preperty. This is used to pass complex elements as children of the property
+          // This whole counter tag is to send children props to the counter object. This can be used for example when we distribute components of dialog boxes to consumers and they can specify their own warning messages in the dialog box components through this children preperty. This is used to pass complex elements as children of the property
           /* <Counter key={counter.id} value={counter.value}>
             <h4>Counter #{counter.id}</h4>
           </Counter> */
+          // We have used value={counter.value} and id={counter.id} above, we can reduce the code here by sending the whole counter variable (declared in map()) to the current counter object
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onDelete={this.handleDelete}
+          />
         ))}
         {/* Map all the items in the counters list of the state object. Map each to a counter object with a key equal to counter id */}
       </div>
