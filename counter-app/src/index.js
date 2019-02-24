faa4b9f9a179052523a0e469b6d9830ab9ca5d0f
shrke <shreyamkela@gmail.com>
2019-02-23 22:55:28 -0800

    Navbar component

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -4,15 +4,15 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Counters from "./components/counters_v1";
+// import Counters from "./components/counters_v1";
 
 // const element = <h1>Hello World</h1>; // This is JSX which is the same as - var element = React.createElement("h1", null, "Hello World"); - Babel converts JSX into the JS language that browser can understand, at compile time. That is, JSX is just syntactical sugar to make developers feel more at home when manipulating html elements from javascript code.
 // console.log(element);
 // // hot module loading - by default, react script is restarted when he hit save. Thus automatically reflects on the browser.
 
 // ReactDOM.render(element, document.getElementById("root")); // render(<what to render>, <where to render>) - root element in the DOM is the container for our whole react app
-
-ReactDOM.render(<Counters />, document.getElementById("root"));
+// ReactDOM.render(<Counters />, document.getElementById("root")); // When there was no nav bar we rendered the counters component directly
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
